4f518d8e97cb0bd8aad3378d0b70f451cb276323
오형욱 <nenonenoberneno@gmail.com>
2021-03-01 22:32:16 +0900

    node.js  URL로 입력된 값 사용하기

---
+++ syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1')  // 11
+console.log('Lorem Ipsum is simply dummy text of the printing and typesetting industry.'.length);  // 74
---
+++ syntax/vareable2.js
@@ -0,0 +1,3 @@
+var name = 'oho'
+var letter = 'Lorem' + name +' Ipsum is simply dummy text of' + name +' the printing and typesetting industry.'
+console.log(letter);
