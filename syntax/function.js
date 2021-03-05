bc7e87ee44e450be969f03191a7a3a1ea700687c
오형욱 <nenonenoberneno@gmail.com>
2021-03-05 23:36:49 +0900

    함수를 이용해서 정리 정돈하기

--- syntax/function.js
+++ syntax/function.js
@@ -21,3 +21,26 @@ function f123(){
   console.log(3);
   console.log(4);
 }
+
+// 함수의 입력
+
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+ 
+function sum(first, second){ // parameter
+  console.log(first+second);
+}
+ 
+sum(2,4); // argument
+
+
+// 함수의 출력
+
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+ 
+function sum(first, second){ // parameter
+  return first+second;
+}
+ 
+console.log(sum(2,4)); // argument
