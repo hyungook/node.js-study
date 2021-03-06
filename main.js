1998f7877a0e107bb2ca9a026ef1dbcd7014f57d
오형욱 <nenonenoberneno@gmail.com>
2021-03-06 15:56:36 +0900

    POST 방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
  
 function templateHTML(title, list, body){
   return `
@@ -63,7 +64,7 @@ var app = http.createServer(function(request,response){
             var title = 'WEB = create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/process_create" method="post">
+            <form action="http://localhost:3000/create_process" method="post">
                 <p><input type = "text" name="title" placeholder="title"></p>
                 <p>
                     <textarea name="discription" placeholder="discription"></textarea>
@@ -76,6 +77,21 @@ var app = http.createServer(function(request,response){
             response.writeHead(200);
             response.end(template);
         });
+    } else if(pathname === '/create_process'){
+
+        let body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post.title);
+        });
+
+        response.writeHead(200);
+        response.end('sucess');
     } else {
       response.writeHead(404);
       response.end('Not found');
