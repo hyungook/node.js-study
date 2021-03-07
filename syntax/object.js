670c93e520f4bf7d5c754c414403435dc213f74a
오형욱 <nenonenoberneno@gmail.com>
2021-03-07 22:55:20 +0900

    javascript - 객체와 형식

---
+++ syntax/object.js
@@ -0,0 +1,14 @@
+// Object
+
+// array
+let member = ['egoing', 'OHO', 'nenonenoberneno'];
+console.log(member[1]); // OHO
+
+
+// object
+let roles = {
+    'programmer':'egoing',
+    'designer' : 'OHO',
+    'manager' : 'hoya'
+};
+console.log(roles.designer); // OHO
