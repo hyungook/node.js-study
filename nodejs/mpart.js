fa9a4c8d5d3d9b2822d779e346fce5265ff1871a
오형욱 <nenonenoberneno@gmail.com>
2021-03-08 22:21:07 +0900

    모듈의 형식

---
+++ nodejs/mpart.js
@@ -0,0 +1,9 @@
+var M = {
+    v: 'v',
+    f: function() {
+        console.log(this.v);
+    }
+}
+
+// 아래 코드는 약속
+module.exports = M;
---
+++ nodejs/muse.js
@@ -0,0 +1,14 @@
+// Module
+
+// var M = {
+//     v: 'v',
+//     f: function() {
+//         console.log(this.v);s
+//     }
+// }
+
+var part = require('./mpart.js');
+// console.log(part);
+
+// M.f();
+part.f();
