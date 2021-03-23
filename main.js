1fff294f1805be1315af69b8c5ce9cd6e5265902
오형욱 <nenonenoberneno@gmail.com>
2021-03-23 23:36:29 +0900

    MySQL로 홈페이지 구현1

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'1q2w3e4r',
+  database:'opentutorials'
+});
+db.connect();
+
 // refactoring
 
 var app = http.createServer(function(request,response){
@@ -14,17 +24,22 @@ var app = http.createServer(function(request,response){
   var pathname = url.parse(_url, true).pathname;
   if(pathname === '/'){
     if(queryData.id === undefined){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list,
-          `<h2>${title}</h2>${description}`,
-          `<a href="/create">create</a>`
-        );
+      // fs.readdir('./data', function(error, filelist){
+      //   var title = 'Welcome';
+      //   var description = 'Hello, Node.js';
+      //   var list = template.list(filelist);
+      //   var html = template.HTML(title, list,
+      //     `<h2>${title}</h2>${description}`,
+      //     `<a href="/create">create</a>`
+      //   );
+      //   response.writeHead(200);
+      //   response.end(html);
+      // });
+      db.query(`SELECT * FROM topic`, function(error, topics) {
+        console.log(topics);
         response.writeHead(200);
-        response.end(html);
-      });
+        response.end('Success');
+      })
     } else {
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
