49b44079cc19998f6973247f8ea17c2829120254
오형욱 <nenonenoberneno@gmail.com>
2021-03-25 17:22:43 +0900

    MySQL로 상세보기 구현_ing

--- main.js
+++ main.js
@@ -24,17 +24,6 @@ var app = http.createServer(function(request,response){
   var pathname = url.parse(_url, true).pathname;
   if(pathname === '/'){
     if(queryData.id === undefined){
-      // fs.readdir('./data', function(error, filelist){
-      //   var title = 'Welcome';
-      //   var description = 'Hello, Node.js';
-      //   var list = template.list(filelist);
-      //   var html = template.HTML(title, list,
-      //     `<h2>${title}</h2>${description}`,
-      //     `<a href="/create">create</a>`
-      //   );
-      //   response.writeHead(200);
-      //   response.end(html);
-      // });
       db.query(`SELECT * FROM topic`, function(error, topics) {
         console.log(topics);
         var title = 'Welcome';
@@ -46,7 +35,7 @@ var app = http.createServer(function(request,response){
         );
         response.writeHead(200);
         response.end(html);
-      })
+      });
     } else {
       // fs.readdir('./data', function(error, filelist){
       //   var filteredId = path.parse(queryData.id).base;
@@ -97,27 +86,29 @@ var app = http.createServer(function(request,response){
             response.end(html);
           });
         })
-        
-
     }
   } else if(pathname === '/create'){
-    fs.readdir('./data', function(error, filelist){
-      var title = 'WEB - create';
-      var list = template.list(filelist);
-      var html = template.HTML(title, list, `
-        <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-      `, '');
-      response.writeHead(200);
-      response.end(html);
-    });
+
+      db.query(`SELECT * FROM topic`, function(error, topics) {
+        console.log(topics);
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `<form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>`,
+          `<a href="/create">create</a>`
+        );
+        response.writeHead(200);
+        response.end(html);
+      });
+
   } else if(pathname === '/create_process'){
     var body = '';
     request.on('data', function(data){
@@ -125,12 +116,19 @@ var app = http.createServer(function(request,response){
     });
     request.on('end', function(){
         var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.writeHead(302, {Location: `/?id=${title}`});
-          response.end();
-        })
+
+        db.query(`
+          INSERT INTO topic (title, description, created, author_id) 
+          VALUES(?, ?, NOW(), ?)`,
+          [post.title, post.description, 1],
+          function(error, result) {
+            if(error) {
+              throw error;
+            }
+            response.writeHead(302, {Location: `/?id=${result.insertID}`});
+            response.end();
+          }
+        )
     });
   } else if(pathname === '/update'){
     fs.readdir('./data', function(error, filelist){
